7e881411c60ab880f0a43f8530b220779b178d9e
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-08-28 20:19:40 +1000

    backend deploy setup

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT3001
+const PORT = process.env.PORT ||3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
