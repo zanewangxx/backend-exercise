7396915abf93aa74dc01c144c199de2045e6d874
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-08-29 15:30:47 +1000

    part3.9-3.11

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = []
 
