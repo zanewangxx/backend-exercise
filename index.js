bc5c2f4c9140b95d32add9ed3835a1dd4aabfcc2
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-08-29 14:12:53 +1000

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const cors = require('cors')
-app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
   {
