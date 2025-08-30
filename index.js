1af59efa68fcd4231c15b6f30eb6dd1d8a617ef2
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-08-30 17:04:27 +1000

    uibuild

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/api/info', (request, response) => {
   response.send(info)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
